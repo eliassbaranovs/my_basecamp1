6afcd4d087bc43d04a9e6501bcd6e1d3f2ec6ed8
olav69 <124511804+olavs69@users.noreply.github.com>
2024-11-04 19:34:59 +0200

    config login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Input from './components/input'
+import Login from './pages/login'
 
 
 function App() {
@@ -14,11 +14,7 @@ function App() {
           count is {count}
         </button>
         <h1 className="bg-red-400">TEST</h1>
-<<<<<<< HEAD
-      
-=======
-        <Input />
->>>>>>> b6846af882df1bf73b2d12ffdcdac2e78ad0c4bf
+        <Login />
       </div>
     </>
   )
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from '../components/button';
+import Input from '../components/input';
+
+const Login = () => {
+  return (
+    <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+      <div className="px-6 py-4">
+          <div className="flex justify-center mx-auto">
+              <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt="" />
+          </div>
+          <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">Welcome Back</h3>
+
+          <p className="mt-1 text-center text-gray-500 dark:text-gray-400">Login or create account</p>
+
+          <form>
+              <div className="w-full mt-4">
+                 <Input inputPlaceholder="Email Address"/>
+              </div>
+
+              <div className="w-full mt-4">
+                <Input inputPlaceholder="Password"/>
+              </div>
+
+              <div className="flex items-center justify-between mt-4">
+                  <a href="#" className="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500">Forget Password?</a>
+                  <Button buttonText="Sign In"/>
+              </div>
+          </form>
+      </div>
+
+      <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+          <span className="text-sm text-gray-600 dark:text-gray-200">Don't have an account? </span>
+
+          <a href="#" className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline">Register</a>
+      </div>
+  </div>
+  );
+};
+
+export default Login;
