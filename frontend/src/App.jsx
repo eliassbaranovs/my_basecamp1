a61e5628b3b84d5afa2067a4d9a15db7ef30e2a0
olav69 <124511804+olavs69@users.noreply.github.com>
2024-11-04 15:20:29 +0200

    add input component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Footer from './components/footer'
+import Input from './components/input'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +13,7 @@ function App() {
           count is {count}
         </button>
         <h1 className="bg-red-400">TEST</h1>
-        <Footer />
+        <Input />
       </div>
     </>
   )
