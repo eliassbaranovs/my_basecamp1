f795d2025d304b75acdb2d822ee26699004c2a93
eliassbaranovs <82933682+eliassbaranovs@users.noreply.github.com>
2024-11-05 22:38:01 +0200

    Updated projectview.jsx to show list of projects

--- frontend/src/pages/ProjectView.jsx
+++ frontend/src/pages/ProjectView.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Project from "../components/Project";
 
 const ProjectView = () => {
-  return (
-    <h1>You have loggend in</h1>
-  )
-}
+  const [projects, setProjects] = useState([]);
 
-export default ProjectView
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/projects", {
+          method: "GET",
+          credentials: "include",
+        });
+        if (!response.ok) throw new Error("Failed to fetch projects");
+
+        const data = await response.json();
+        setProjects(data);
+      } catch (err) {
+        console.error("Error fetching projects:", err);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        {projects.map((project) => (
+          <Project
+            key={project._id}
+            name={project.name}
+            description={project.description}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectView;
