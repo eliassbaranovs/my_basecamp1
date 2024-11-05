41d03138c849038af5a9b2f7e75e7900eb0f4bd6
Chamis9 <raivis.ogorodovs@gmail.com>
2024-11-05 22:59:45 +0200

    AdminView update

--- frontend/src/pages/AdminView.jsx
+++ frontend/src/pages/AdminView.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const AdminView = () => {
+const AdminView = ({ isAdmin, fetchUsers, assignRole }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (isAdmin) {
+      fetchUsers().then(setUsers);
+    }
+  }, [isAdmin, fetchUsers]);
+
+  const handleRoleChange = (userId, newRole) => {
+    assignRole(userId, newRole).then(() => {
+      setUsers(users.map(user => 
+        user.id === userId ? { ...user, role: newRole } : user
+      ));
+    });
+  };
+
+  if (!isAdmin) {
+    return <div>You do not have permission to view this page.</div>;
+  }
+
   return (
-    <div>AdminView</div>
-  )
-}
+    <div>
+      <h1>Admin View</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name}
+            <select
+              value={user.role}
+              onChange={(e) => handleRoleChange(user.id, e.target.value)}
+            >
+              <option value="user">User</option>
+              <option value="admin">Admin</option>
+            </select>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default AdminView
+export default AdminView;
