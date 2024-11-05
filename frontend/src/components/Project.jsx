48e4e6bd1a1fe4449aabfbe32ad3094726e4831c
eliassbaranovs <82933682+eliassbaranovs@users.noreply.github.com>
2024-11-05 22:44:26 +0200

    Project.jsx changes

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -1,22 +1,4 @@
-import  { useEffect, useState } from 'react';
-import axios from 'axios';
-
 const Project = ({name, description}) => {
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await axios.get('/projects');
-        setProjects(response.data);
-      } catch (error) {
-        console.error('Failed to fetch projects', error);
-      }
-    };
-
-    fetchProjects();
-  }, []);
-
   return (  
           <li key={name}>
             <h2>{name}</h2>
